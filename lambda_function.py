adbdfd6cd7a5717131b521ff3bb7101b2c771523
davidevaristo91 <david.evaristo@altbank.co>
2023-05-20 23:46:52 -0300

    Transformando função em API

--- lambda_function.py
+++ lambda_function.py
@@ -5,5 +5,8 @@ def lambda_handler(event, context):
     log(f"event {json.dumps(event)}")
     return {
         'statusCode': 200,
-        'body': json.dumps(event)
+        'body': f'<html><body>Dados da requisicao ${json.dumps(event)}</body></html>',
+        'headers': {
+            "content-type": "text/html"
+        }
     }
