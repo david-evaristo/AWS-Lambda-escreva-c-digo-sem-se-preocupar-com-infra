945660a5b08fea362460066240de859584d8d89b
davidevaristo91 <david.evaristo@altbank.co>
2023-05-21 00:49:21 -0300

    Teste S3 Client

--- lambda_function.py
+++ lambda_function.py
@@ -10,3 +10,29 @@ def lambda_handler(event, context):
             "content-type": "text/html"
         }
     }
+
+# import json
+# import logging
+# import boto3
+# from log import log
+# from botocore.exceptions import ClientError
+# def lambda_handler(event, context):
+#     s3_name = event['Records'][0]['s3']['bucket']['name']
+#     s3_key = event['Records'][0]['s3']['object']['key']
+    
+#     print(s3_name,s3_key)
+#     # Recupere o objeto S3 usando o nome do objeto e o bucket S3
+#     s3_client = boto3.client('s3')
+#     s3_object = s3_client.get_object(Bucket=s3_name, Key=s3_key)
+    
+#     # Carregue o conteúdo do objeto S3 como JSON
+#     json_content = s3_object['Body'].read().decode('utf-8')
+#     json_data = json.loads(json_content)
+#     print(json_data)
+#     return {
+#         'statusCode': 200,
+#         'body': f'<html><body>Dados da requisicao {event}</body></html>',
+#         'headers': {
+#             "content-type": "text/html"
+#         }
+#     }
