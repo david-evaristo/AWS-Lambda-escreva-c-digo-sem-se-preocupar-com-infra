c7c31dd00afea9ec16710a20ecc4d2d140f3bbe5
davidevaristo91 <david.evaristo@altbank.co>
2023-05-20 22:40:46 -0300

    Alterando LOG

--- lambda_function.py
+++ lambda_function.py
@@ -2,7 +2,7 @@ import json
 from log import log
 
 def lambda_handler(event, context):
-    log(f"Log de execução {json.dumps(event)}")
+    log(f"Log de execução apos github action {json.dumps(event)}")
     return {
         'statusCode': 200,
         'body': json.dumps(event)
